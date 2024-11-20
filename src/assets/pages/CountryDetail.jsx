46c859ec85b5db009eecd4e4d623f0d5690f25c0
Dimitris_Dr <jidrist18@hotmail.com>
2024-11-20 10:35:26 +0200

    icons update

--- src/assets/pages/CountryDetail.jsx
+++ src/assets/pages/CountryDetail.jsx
@@ -27,7 +27,7 @@ export default function CountryDetail() {
     <section className='country-data-container'>
         <div className="container">
             <Link className='back-btn link flex-item' to ='/rest-countries-api/'>
-                <i className="fa-solid fa-arrow-left"></i>
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z"/></svg>
                 Back 
             </Link>
             <div className="country-data grid-item container">
