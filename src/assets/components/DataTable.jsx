874b74eb0af220f084c58dec641d494a840ed58b
Dimitris_Dr <jidrist18@hotmail.com>
2024-11-20 09:23:04 +0200

    updating the readme.md

--- src/assets/components/DataTable.jsx
+++ src/assets/components/DataTable.jsx
@@ -1,12 +1,12 @@
 export default function DataTable({data}) {
     return (
         <div className="country-info__table grid-item">
-                {data.map(row => 
-                    <div key={[Object.keys(row)[0]]} className="table-row flex-item">
-                        <span className="table-cell fw-600">{Object.keys(row)}: </span>
-                        <span className="table-cell table-cell-info text-ellipsis">{Object.values(row)}</span>
-                    </div>
-                )}
+            {data.map(row => 
+                <div key={[Object.keys(row)[0]]} className="table-row flex-item">
+                    <span className="table-cell fw-600">{Object.keys(row)}: </span>
+                    <span className="table-cell table-cell-info text-ellipsis">{Object.values(row)}</span>
+                </div>
+            )}
         </div>
     )
 };
