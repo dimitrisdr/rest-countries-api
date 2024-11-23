25913f7287e044abf0edb65293c85927a027cd3f
Dimitris_Dr <jidrist18@hotmail.com>
2024-11-23 14:49:26 +0200

    update i dont know what

--- src/assets/components/SelectGroup.jsx
+++ src/assets/components/SelectGroup.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import InputItem from "./InputItem"
 
 
@@ -7,9 +7,27 @@ export default function SelectGroup({filterData, setFilterData}) {
     const data = ['All Regions', 'Africa', 'America', 'Europe', 'Oceania']
     const [isOpened, setIsOpened] = useState(false)
     const [isSelected, setIsSelected] = useState('All Regions')
+    const isOptionsClosedRef = useRef()
+
+    useEffect(()=> {
+        function handleOutsideClick(e) {
+            if (isOpened === false) {
+                return
+            } else {
+                if (e.target !== isOptionsClosedRef.current){
+                    console.log('options not clicked')
+                    setIsOpened(false)
+                }else {
+                    console.log('options clicked')
+                }
+            }
+        }
+        document.addEventListener('mousedown', handleOutsideClick)
+        return () => document.removeEventListener('mousedown', handleOutsideClick)
+    }, [isOpened])
 
     return (
-        <div className="select-container grid-item tools-item">
+        <div ref={isOptionsClosedRef} className="select-container grid-item tools-item">
             <button 
             className="btn input-btn flex-item"
             onClick={()=> setIsOpened(!isOpened)}
